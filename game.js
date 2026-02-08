9b6c9bcb3d31441709704c5df9f6c8b3467524b4
ItsyBOT <itsybot@itsybit.dev>
2026-02-08 18:28:30 +0100

    Mr. White starts first (fair trade for getting category hint)

--- game.js
+++ game.js
@@ -149,15 +149,15 @@ function assignRoles() {
         players[shuffledIndices[1]].word = gameState.undercoverWord;
     }
     
-    // Determine play order - civilians first, then others
-    // Shuffle all player indices, then move a civilian to the front
+    // Determine play order - Mr. White starts (since they get a category hint)
+    // Shuffle all player indices, then move Mr. White to the front
     gameState.playOrder = shuffle([...Array(players.length).keys()]);
     
-    // Find first civilian in the shuffled order and move to front
-    const firstCivilianIdx = gameState.playOrder.findIndex(i => players[i].role === 'civilian');
-    if (firstCivilianIdx > 0) {
-        const civilian = gameState.playOrder.splice(firstCivilianIdx, 1)[0];
-        gameState.playOrder.unshift(civilian);
+    // Find Mr. White and move to front
+    const mrWhiteIdx = gameState.playOrder.findIndex(i => players[i].role === 'mrwhite');
+    if (mrWhiteIdx > 0) {
+        const mrWhite = gameState.playOrder.splice(mrWhiteIdx, 1)[0];
+        gameState.playOrder.unshift(mrWhite);
     }
 }
 
